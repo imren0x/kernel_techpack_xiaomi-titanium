f72e082f89a67b2b8fec84abf1accf84f77283f5
Aqur3nz <enashinonome264@gmail.com>
2026-03-08 14:00:33 +0000

    techpack: titanium: ts: ft5446: enable fts esdcheck

--- touchscreen/ft5446/focaltech_config.h
+++ touchscreen/ft5446/focaltech_config.h
@@ -205,7 +205,7 @@
  * ESD check & protection
  * default: disable
  */
-#define FTS_ESDCHECK_EN                         0
+#define FTS_ESDCHECK_EN                         1
 
 /*
  * Production test enable
